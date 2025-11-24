7a3c27948385cdae21432d23fad705a44a29605e
annaivv <annashalya@gmail.com>
2025-11-24 16:18:32 +0100

    feature: stories for toast added

--- src/components/Toast/Toast.stories.tsx
+++ src/components/Toast/Toast.stories.tsx
@@ -22,3 +22,63 @@ export const InfoToast: Story = {
     message: "We have some info for you",
   },
 };
+
+export const InfoToastClosing: Story = {
+  render: (args: ToastProps) => <Toast {...args} />,
+  args: {
+    message: "We have info for you",
+    duration: 5000,
+    showCloseButton: true,
+  },
+};
+
+export const SuccessToast: Story = {
+  render: (args: ToastProps) => <Toast {...args} />,
+  args: {
+    message: "Completed successfully!",
+    type: "success",
+  },
+};
+
+export const SuccessToastClosing: Story = {
+  render: (args: ToastProps) => <Toast {...args} />,
+  args: {
+    message: "Completed successfully!",
+    type: "success",
+    showCloseButton: true,
+  },
+};
+
+export const WarningToast: Story = {
+  render: (args: ToastProps) => <Toast {...args} />,
+  args: {
+    message: "Watch out for this!",
+    type: "warning",
+  },
+};
+
+export const WarningToastLonger: Story = {
+  render: (args: ToastProps) => <Toast {...args} />,
+  args: {
+    message: "Watch out for this!",
+    type: "warning",
+    duration: 5000,
+  },
+};
+
+export const ErrorToast: Story = {
+  render: (args: ToastProps) => <Toast {...args} />,
+  args: {
+    message: "An error occured",
+    type: "error",
+  },
+};
+
+export const ErrorToastLonger: Story = {
+  render: (args: ToastProps) => <Toast {...args} />,
+  args: {
+    message: "An error occured",
+    type: "error",
+    duration: 6000,
+  },
+};
